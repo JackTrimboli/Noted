19219e796be6a11bf08e80b671b5108cfd1af0d1
JACK TRIMBOLI <Jack.Trimboli@yahoo.com>
2021-01-03 20:42:23 -0500

    quick fix

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
+import { StatusBar } from 'expo-status-bar'
+import React, { Component } from 'react'
 import * as firebase from 'firebase'
 
-require('dotenv').config()
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_API_KEY",
-  authDomain: "noted-demo.firebaseapp.com",
-  projectId: "noted-demo",
-  storageBucket: "noted-demo.appspot.com",
-  messagingSenderId: "374976912400",
-  appId: "1:374976912400:web:6e9d1a2fcf864b9d113e95",
-  measurementId: "G-16QWP43EDJ"
-};
 
-
+import { firebaseConfig } from './firebase-config.js'
 if(firebase.apps.length === 0){
   firebase.initializeApp(firebaseConfig)
 }
-import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import LandingScreen from './components/auth/Landing';
-import RegisterScreen from './components/auth/register';
-import LoginScreen from './components/auth/Login';
+
+import { StyleSheet, Text, View } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/register'
+import LoginScreen from './components/auth/Login'
 
 const Stack = createStackNavigator();
 
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCeEqToJFVlFUTkVPdLNRNw32FLzj7Z2wM",
+    authDomain: "noted-demo.firebaseapp.com",
+    projectId: "noted-demo",
+    storageBucket: "noted-demo.appspot.com",
+    messagingSenderId: "374976912400",
+    appId: "1:374976912400:web:6e9d1a2fcf864b9d113e95",
+    measurementId: "G-16QWP43EDJ"
+  };
